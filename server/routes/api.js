d3e41ce86f13f00b34b6803bf39029fb18a2cef0
unknown <gbsoares12@gmail.com>
2019-07-04 10:03:55 -0300

    commit deploy frontend

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 /* GET api listing. */
 router.get('/', (req, res) => {
-    res.send('api works');
+    res.sendFile(path.join(__dirname, '../dist/projetoRota/index.html'));
 });
 
 module.exports = router;
